c4ee10834428ffbce7f1ea26fdb5830e625757c3
Oleksandra <diamond210390@gmail.com>
2024-01-17 14:41:46 +0200

    fetch-data

--- code/script.js
+++ code/script.js
@@ -19,35 +19,46 @@ function getCurrentDate() {
 
 input.addEventListener('input', () => {
     request(input.value);
-    
 })
 
 async function request(value) {
 
     try {
         loadingIndicator.style.display = 'block';
+        
         const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&q=${value}&appid=5868385811e81ae08671f43616141be1`, {
             method: 'GET',
         });
+        console.log(data.ok)
         const response = await data.json();
         loadingIndicator.style.display = 'none';
         btn.addEventListener('click', () => {
-            getWeather(response)
+            if(data.ok){
+                getWeather(response)
+                input.value = '';
+            }else{
+                input.value = '';
+                weatherInfo.style.display = 'none';
+            }
+            
         });
     } catch (error) {
         console.error('Помилка при отриманні даних:', error);
     }
 }
 function getWeather(data) {
-    console.log(data);
-    input.value = '';
-    weatherInfo.style.display = 'block';
-    city.innerHTML = data.name;
-    weatherStatus.innerHTML = data.weather[0].main;
-    weatherIcon.src = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-    weatherIcon.alt = data.weather[0].main;
-    temperature.innerHTML = `${Math.round(data.main.temp)} °C`;
-    feelsLike.innerHTML = `Feels like: ${Math.round(data.main.feels_like)} °C`;
+    if(data){
+        console.log(data);
+
+        weatherInfo.style.display = 'block';
+        city.innerHTML = data.name;
+        weatherStatus.innerHTML = data.weather[0].main;
+        weatherIcon.src = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+        weatherIcon.alt = data.weather[0].main;
+        temperature.innerHTML = `${Math.round(data.main.temp)} °C`;
+        feelsLike.innerHTML = `Feels like: ${Math.round(data.main.feels_like)} °C`; 
+    }
+
 }
 
 
